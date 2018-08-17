129814127350fbcfea7436bf5be510b0a005beb5
Violet Moon <violet.wmoon@gmail.com>
2018-08-17 08:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ var recipes = {
 
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-    return object.key = value + 1
-}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-    return object.key = value
-}
